ae49ed6963fffba979beb544bf913754ab319fbc
mjeda <abdulrazak.amd@gmail.com>
2024-04-11 20:08:54 +0300

    Register supplier form

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
         <Stack.Screen name="SearchSupplier" component={SearchScreen} options={{title: "Search supplier"}}></Stack.Screen>
-        <Stack.Screen name="AddSupplier" component={RegisterSupplierScreen} options={{title: "Search supplier", headerShown: true}}></Stack.Screen>
+        <Stack.Screen name="AddSupplier" component={RegisterSupplierScreen} options={{title: "Register Supplier", headerShown: true}}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/screens/RegisterSupplierScreen.jsx
+++ app/screens/RegisterSupplierScreen.jsx
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TextInput, Button } from "react-native";
+import React from "react";
 
 export default function RegisterSupplierScreen() {
   return (
-    <View>
-      <Text>RegisterSupplierScreen</Text>
-    </View>
-  )
+    <SafeAreaView className="flex-1 bg-gray-100 p-5">
+      <View className="flex-1 bg-white rounded-lg shadow-md p-4 mb-4">
+        <Text className="text-lg pb-1">Name</Text>
+        <TextInput
+          placeholder="Enter name"
+          className="py-2 border border-gray-300 px-4 mb-2"
+        />
+
+        <Text className="text-lg pb-1">Phone Number</Text>
+        <TextInput
+          placeholder="Enter phone number"
+          className="py-2 border border-gray-300 px-4 mb-2"
+        />
+
+        <Button title="Save" className="bg-blue-300 mx-4" />
+      </View>
+    </SafeAreaView>
+  );
 }
